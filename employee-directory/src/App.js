e839e4d8f8ffdf83510c1d7e67e8398806993701
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-12-01 23:01:05 -0600

    adding route functionality. Still need to work on refining and getting functionality. Debugging

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,34 +1,78 @@
-import './App.css';
+import './styles/App.css';
 import React from 'react';
-import EmployeeLinks from './Components/Employee-links';
-import Jumbo from './Components/Jumbotron';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import employee from './employees.json';
+import { ListGroup, ListGroupItem, Button, Jumbotron, Container, Row, Col } from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route, Link, BrowserRouter} from 'react-router-dom';
+import employees from './employees.json';
+import EmployeeTable from './Components/EmployeeTable';
+import DepartmentJumbo from './Components/DepartmentJumbotron'
 
 class App extends React.Component {
  state = {
-   employee
+   employees: employees,
+   display: 'home'
+ }
+
+ showEmployeeTable() {
+   this.setState({display: this.state.display = 'viewall'})
  }
 
  render() {
    return (
-    <div className="App">
-    <Container>
-      <Row>
-        <Col className="jumboTron">
-          <Jumbo/>
-        </Col>       
+     <BrowserRouter>
+      {this.state.display === "home" && (
+        <div className="App">        
+        <Container>
+          <Row>
+            <Col className="jumboTron">
+              <Jumbotron fluid>
 
-      </Row>
-      <Row>
+                  <h1>Welcome to your Employee Directory!</h1>
 
-        <Col className="sideLinks">
-          <EmployeeLinks/>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+                  <hr/>
+
+                  <p>Select from the list below to select what you would like to do.</p>
+
+
+              </Jumbotron>
+            </Col>       
+
+          </Row>
+          <Row>
+
+            <Col className="sideLinks">
+              <div className="employeeList">
+                  <ListGroup>
+                      <ListGroupItem><Link to="/departments"><Button variant="outline-primary" block>View Departments</Button></Link></ListGroupItem>
+                      <ListGroupItem><Link to="/viewall"><Button variant="outline-primary" block>View All Employees</Button></Link></ListGroupItem>
+                      <ListGroupItem><Link to="/search"><Button variant="outline-primary" block
+                      onClick={this.showEmployeeTable()}
+                      >Search Employees</Button></Link></ListGroupItem>
+                      <ListGroupItem><Link to="/add"><Button variant="outline-primary" block>Add Employees</Button></Link></ListGroupItem>
+                      <ListGroupItem><Link to="/remove"><Button variant="outline-primary" block>Remove Employees</Button></Link></ListGroupItem>
+                  </ListGroup>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      )}
+
+      {this.state.display === "viewall" && (
+        {EmployeeTable}
+      )}
+      
+      <Switch>
+        <Route exact path="/"/>
+        <Route exact path="/departments" component={DepartmentJumbo}/>
+        <Route exact path="/viewall" component={EmployeeTable}/>
+        <Route exact path="/search"/>
+        <Route exact path="/add"/>
+        <Route exact path="/remove"/>
+      </Switch>
+
+
+     </BrowserRouter>
    )
  }
 }
--- employee-directory/src/Components/EmployeeTable.jsx
+++ employee-directory/src/Components/EmployeeTable.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Table, Card, ListGroup } from 'react-bootstrap'
+import { Table, Card, ListGroup } from 'react-bootstrap';
+import employees from '../employees.json'
 
-const EmployeeTable = (props) => {
-    return(
-        <Card style={{ width: '18rem'}}>
-            <Card.Body>
-                <Card.Title>{props.firstname} {props.lastname}</Card.Title>
-                <Card.Text>
-                    <ListGroup.Item>Department: {props.department}</ListGroup.Item>
-                    <ListGroup.Item>Email: {props.email}</ListGroup.Item>
-                </Card.Text>                
-            </Card.Body>
-        </Card>
-    )
+class EmployeeTable extends React.Component {
+    state= {
+        employees: employees
+    }
+
+    render() {
+        return(
+            this.state.employees.map((employee) => 
+            <div>
+            <Card style={{ width: '18rem'}}>
+                <Card.Body>
+                    <Card.Title>{employee.firstname} {employee.lastname}</Card.Title>
+                    <Card.Text>
+                        <ListGroup.Item>Department: {employee.department}</ListGroup.Item>
+                        <ListGroup.Item>Email: {employee.email}</ListGroup.Item>
+                    </Card.Text>                
+                </Card.Body>
+            </Card>
+        </div>)
+            
+        )
+    }
 }
 
 export default EmployeeTable
